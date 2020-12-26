7f1c840441761ec43e5e09ce41ad18023ce4a71b
Gibran Mather <gibranmather@gmail.com>
2020-12-26 00:07:14 +0000

    walleye: Clean up and correct paths

--- aosp_walleye.mk
+++ aosp_walleye.mk
@@ -19,8 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 
-$(call inherit-product, device/google/muskie/device-walleye.mk)
-$(call inherit-product-if-exists, vendor/google_devices/muskie/proprietary/device-vendor-walleye.mk)
+$(call inherit-product, device/google/walleye/device-walleye.mk)
 
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0 \
@@ -30,9 +29,9 @@ PRODUCT_PACKAGES += \
     vndk_package
 
 PRODUCT_COPY_FILES += \
-    device/google/muskie/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
+    device/google/walleye/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
 
-PRODUCT_RESTRICT_VENDOR_FILES := owner
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
 
